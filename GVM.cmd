f4338c7beec5225abf92050190202dd5584dabf9
Andy Willis <awillis1@us.ibm.com>
2016-11-15 16:19:33 -0600

    Add @ to echo to stop echo off from showing.

--- GVM.cmd
+++ GVM.cmd
@@ -1,6 +1,6 @@
-echo off
+@echo off
 REM Copyright Andy Willis Licensed to IBM 
-REM Version 1.0
+REM Version 1.0.1
 if exist validatemefs.vbs del validatemefs.vbs
 if exist info.txt del info.txt
 if exist %USERPROFILE%\Downloads\validatemefs.vbs del %USERPROFILE%\Downloads\validatemefs.vbs
