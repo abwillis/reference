91dd6ecdf4f30093bdc59c4a641dce3e03e97e0d
Andy Willis <awillis1@us.ibm.com>
2016-11-16 08:04:19 -0600

    Added findstr for Nomefs and fixed csv filename selection.

--- GVM.cmd
+++ GVM.cmd
@@ -18,7 +18,28 @@ start notepad info.txt
 cscript validatemefs.vbs
 Choice /M "Do you want to open the csv file? (Y/N)"
 IF ERRORLEVEL == 2 goto nocsv
-"C:\Program Files (x86)\Microsoft Office\root\Office16\excel.exe" validatemefs_*_REPORT.csv
+FOR /F "tokens=*" %%A IN ('DATE/T') DO FOR %%B IN (%%A) DO SET Today=%%B
+SET result=%Today:*/=%
+SET year=%result:*/=%
+CALL SET intermediate=%%result:%year%=%%
+SET day=%intermediate:/=%
+SET first=%Today:*/=%
+CALL SET intermediate=%%Today:%first%=%%
+set month=%intermediate:/=%
+if %month%==1 set month=Jan
+if %month%==2 set month=Feb
+if %month%==3 set month=Mar
+if %month%==4 set month=Apr
+if %month%==5 set month=May
+if %month%==6 set month=Jun
+if %month%==7 set month=Jul
+if %month%==8 set month=Aug
+if %month%==9 set month=Sep
+if %month%==10 set month=Oct
+if %month%==11 set month=Nov
+if %month%==12 set month=Dec
+findstr NOMEFS validatemefs_%day%%month%%year%_REPORT.csv
+"C:\Program Files (x86)\Microsoft Office\root\Office16\excel.exe" validatemefs_%day%%month%%year%_REPORT.csv
 :nocsv
 goto end
 
