eb4a75554fc7e5d6746c80bc4d722dc21a52844f
Andy Willis <awillis1@us.ibm.com>
2016-11-18 15:55:37 -0600

    Added date, inventory and account name prompt for filename and open with open office if available, else excel.

--- ecminv.cmd
+++ ecminv.cmd
@@ -1,10 +1,9 @@
-@REM Version 1.5 ECM csv Inventory cleanup
+@REM Version 2.0 ECM csv Inventory cleanup
 @echo off
 set filename=%1
 if [%1]==[] set /P filename=Enter filename 
 
 del newfile.csv
-del complete.csv
 for /f "skip=3 delims=*" %%a in (%filename%) do (
 echo %%a >>newfile.csv   
 )
@@ -25,6 +24,30 @@ for /F "tokens=*" %%A in (newfile.csv) do (
 		set column[6]=%%g
 		set column[7]=%%h
 )
-        echo !column[0]!;!column[1]!;!column[2]!;!column[3]!;!column[4]!;!column[5]!;!column[6]!;!column[7]!;!column[8]!;  >>complete.csv
+        FOR /F "tokens=*" %%A IN ('DATE/T') DO FOR %%B IN (%%A) DO SET Today=%%B
+			SET result=%Today:*/=%
+			SET year=%result:*/=%
+			CALL SET intermediate=%%result:%year%=%%
+			SET day=%intermediate:/=%
+			SET first=%Today:*/=%
+			CALL SET intermediate=%%Today:%first%=%%
+			set month=%intermediate:/=%
+			if %month%==1 set month=Jan
+			if %month%==2 set month=Feb
+			if %month%==3 set month=Mar
+			if %month%==4 set month=Apr
+			if %month%==5 set month=May
+			if %month%==6 set month=Jun
+			if %month%==7 set month=Jul
+			if %month%==8 set month=Aug
+			if %month%==9 set month=Sep
+			if %month%==10 set month=Oct
+			if %month%==11 set month=Nov
+			if %month%==12 set month=Dec
+			echo !column[0]!;!column[1]!;!column[2]!;!column[3]!;!column[4]!;!column[5]!;!column[6]!;!column[7]!;!column[8]!;  >>Storage_%day%%month%%year%Inventory.csv
 )
+set /P Account=Account Name 
+ren Storage_%day%%month%%year%Inventory.csv %Account%_Storage_%day%%month%%year%_Inventory.csv
+IF EXIST "\Program Files (x86)\OpenOffice 4\program\scalc.exe" Start "\Program Files (x86)\OpenOffice 4\program\scalc.exe" %Account%_Storage_%day%%month%%year%_Inventory.csv
+IF NOT EXIST "\Program Files (x86)\OpenOffice 4\program\scalc.exe" start "C:\Program Files (x86)\Microsoft Office\root\Office16\excel.exe" %Account%_Storage_%day%%month%%year%_Inventory.csv
 endlocal
