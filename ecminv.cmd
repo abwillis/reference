7d9f8b08df530fe61c837a307ee9afde9bbe2a58
Andy Willis <awillis1@us.ibm.com>
2016-11-18 16:50:46 -0600

    Added preserve file, added new date tool, fixed typos.

--- ecminv.cmd
+++ ecminv.cmd
@@ -1,16 +1,16 @@
-@REM Version 2.0 ECM csv Inventory cleanup
+@REM Version 2.1 ECM csv Inventory cleanup
 @echo off
 set filename=%1
 if [%1]==[] set /P filename=Enter filename 
 
 del newfile.csv
+del Storage_complete_Inventory.csv
 for /f "skip=3 delims=*" %%a in (%filename%) do (
 echo %%a >>newfile.csv   
 )
 setlocal enableextensions enabledelayedexpansion
 for /F "tokens=*" %%A in (newfile.csv) do (
     set LINE="%%A"
-	set test=""
     for /F "tokens=2-7,21,26,* delims=;" %%a in (!LINE!) do (
 		for /F "tokens=6 delims=;" %%Z in ("%%i") do (
 			set column[8]=%%Z
@@ -31,23 +31,27 @@ for /F "tokens=*" %%A in (newfile.csv) do (
 			SET day=%intermediate:/=%
 			SET first=%Today:*/=%
 			CALL SET intermediate=%%Today:%first%=%%
-			set month=%intermediate:/=%
-			if %month%==1 set month=Jan
-			if %month%==2 set month=Feb
-			if %month%==3 set month=Mar
-			if %month%==4 set month=Apr
-			if %month%==5 set month=May
-			if %month%==6 set month=Jun
-			if %month%==7 set month=Jul
-			if %month%==8 set month=Aug
-			if %month%==9 set month=Sep
-			if %month%==10 set month=Oct
-			if %month%==11 set month=Nov
-			if %month%==12 set month=Dec
-			echo !column[0]!;!column[1]!;!column[2]!;!column[3]!;!column[4]!;!column[5]!;!column[6]!;!column[7]!;!column[8]!;  >>Storage_%day%%month%%year%Inventory.csv
+			set tmonth=%intermediate:/=%
+			set track="Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec"
+			for /F "tokens=%tmonth% delims=," %%y in (%track%) do (
+				set month=%%y
+			)
+			echo !column[0]!;!column[1]!;!column[2]!;!column[3]!;!column[4]!;!column[5]!;!column[6]!;!column[7]!;!column[8]!;  >>Storage_complete_Inventory.csv
 )
 set /P Account=Account Name 
-ren Storage_%day%%month%%year%Inventory.csv %Account%_Storage_%day%%month%%year%_Inventory.csv
-IF EXIST "c:\Program Files (x86)\OpenOffice 4\program\scalc.exe" Start "c:\Program Files (x86)\OpenOffice 4\program\scalc.exe" %Account%_Storage_%day%%month%%year%_Inventory.csv
+        FOR /F "tokens=*" %%A IN ('DATE/T') DO FOR %%B IN (%%A) DO SET Today=%%B
+			SET result=%Today:*/=%
+			SET year=%result:*/=%
+			CALL SET intermediate=%%result:%year%=%%
+			SET day=%intermediate:/=%
+			SET first=%Today:*/=%
+			CALL SET intermediate=%%Today:%first%=%%
+			set tmonth=%intermediate:/=%
+			set track="Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec"
+			for /F "tokens=%tmonth% delims=," %%y in (%track%) do set month=%%y
+echo %day%
+if exist %Account%_Storage_%day%%month%%year%_Inventory.csv ren %Account%_Storage_%day%%month%%year%_Inventory.csv %Account%_Storage_%day%%month%%year%_Inventory-%RANDOM%.csv
+ren Storage_complete_Inventory.csv %Account%_Storage_%day%%month%%year%_Inventory.csv
+IF EXIST "c:\Program Files (x86)\OpenOffice 4\program\scalc.exe" "c:\Program Files (x86)\OpenOffice 4\program\scalc.exe" %Account%_Storage_%day%%month%%year%_Inventory.csv
 IF NOT EXIST "c:\Program Files (x86)\OpenOffice 4\program\scalc.exe" start "C:\Program Files (x86)\Microsoft Office\root\Office16\excel.exe" %Account%_Storage_%day%%month%%year%_Inventory.csv
 endlocal
