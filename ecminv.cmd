7797fd1079f2687b475584771129394f94f97fdd
Andy Willis <awillis1@us.ibm.com>
2016-11-21 11:09:36 -0600

    Fix launch of openoffice to not pause command prompt while openoffice is open.

--- ecminv.cmd
+++ ecminv.cmd
@@ -1,4 +1,4 @@
-@REM Version 2.5.5 ECM csv Inventory cleanup
+@REM Version 2.6 ECM csv Inventory cleanup
 @echo off
 set filename=%1
 if [%1]==[] set /P filename=Enter filename 
@@ -40,6 +40,8 @@ set /P Account=Account Name
 			for /F "tokens=%tmonth% delims=," %%y in (%track%) do set month=%%y
 if exist %Account%_Storage_%day%%month%%year%_Inventory.csv ren %Account%_Storage_%day%%month%%year%_Inventory.csv %Account%_Storage_%day%%month%%year%_Inventory-%RANDOM%.csv
 ren Storage_complete_Inventory.csv %Account%_Storage_%day%%month%%year%_Inventory.csv
-IF EXIST "c:\Program Files (x86)\OpenOffice 4\program\scalc.exe" "c:\Program Files (x86)\OpenOffice 4\program\scalc.exe" %Account%_Storage_%day%%month%%year%_Inventory.csv
+IF EXIST "c:\Program Files (x86)\OpenOffice 4\program\scalc.exe" (
+	start /b call "c:\Program Files (x86)\OpenOffice 4\program\scalc.exe" %Account%_Storage_%day%%month%%year%_Inventory.csv
+	)
 IF NOT EXIST "c:\Program Files (x86)\OpenOffice 4\program\scalc.exe" start "C:\Program Files (x86)\Microsoft Office\root\Office16\excel.exe" %Account%_Storage_%day%%month%%year%_Inventory.csv
 endlocal
--- open.cmd
+++ open.cmd
@@ -1,1 +1,8 @@
-powershell ii .
+@REM Open directory
+@REM Version 1.0
+@echo off
+if [%1]==[] (powershell ii .
+	) Else (
+		powershell ii %1
+		cd %1
+	)
