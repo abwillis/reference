1e57169f08609aeb65938a3ee170ee4f6c137c8e
Andy Willis <awillis1@us.ibm.com>
2016-11-21 10:33:13 -0600

    Removed extra date code that was unneeded and caused spurious error messages on screen.

--- ecminv.cmd
+++ ecminv.cmd
@@ -1,4 +1,4 @@
-@REM Version 2.5.01 ECM csv Inventory cleanup
+@REM Version 2.5.5 ECM csv Inventory cleanup
 @echo off
 set filename=%1
 if [%1]==[] set /P filename=Enter filename 
@@ -24,18 +24,7 @@ for /F "tokens=*" %%A in (newfile.csv) do (
 		set column[6]=%%g
 		set column[7]=%%h
 )
-        FOR /F "tokens=*" %%A IN ('DATE/T') DO FOR %%B IN (%%A) DO SET Today=%%B
-			SET result=%Today:*/=%
-			SET year=%result:*/=%
-			CALL SET intermediate=%%result:%year%=%%
-			SET day=%intermediate:/=%
-			SET first=%Today:*/=%
-			CALL SET intermediate=%%Today:%first%=%%
-			set tmonth=%intermediate:/=%
-			set track="Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec"
-			for /F "tokens=%tmonth% delims=," %%y in (%track%) do (
-				set month=%%y
-			)
+
 			echo !column[0]!;!column[1]!;!column[2]!;!column[3]!;!column[4]!;!column[5]!;!column[6]!;!column[7]!;!column[8]!;  >>Storage_complete_Inventory.csv
 )
 set /P Account=Account Name 
@@ -49,7 +38,6 @@ set /P Account=Account Name
 			set tmonth=%intermediate:/=%
 			set track="Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec"
 			for /F "tokens=%tmonth% delims=," %%y in (%track%) do set month=%%y
-echo %day%
 if exist %Account%_Storage_%day%%month%%year%_Inventory.csv ren %Account%_Storage_%day%%month%%year%_Inventory.csv %Account%_Storage_%day%%month%%year%_Inventory-%RANDOM%.csv
 ren Storage_complete_Inventory.csv %Account%_Storage_%day%%month%%year%_Inventory.csv
 IF EXIST "c:\Program Files (x86)\OpenOffice 4\program\scalc.exe" "c:\Program Files (x86)\OpenOffice 4\program\scalc.exe" %Account%_Storage_%day%%month%%year%_Inventory.csv
