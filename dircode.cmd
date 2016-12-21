960d1290b9ec9a33d96b23040d36c5e2d8b59e98
Andy Willis <awillis1@us.ibm.com>
2016-12-21 13:52:14 -0600

    Update dircode to allow current directory with a dot and added genpar parameter to create genpar directories.

--- dircode.cmd
+++ dircode.cmd
@@ -1,20 +1,33 @@
 @REM Create Directories based on account and quarter/year.
-@REM Version 1.0.01
+@REM Version 2.2
 @echo off
 if /I "%1" == "" goto usage
+if /I "%1" == "genpar" goto usage
 if /I "%2" == "" goto usage
+if /I "%2" == "genpar" goto usage
+if /I "%1" == "." goto dotted
 cd c:\accounts
 if exist %1 goto next
 md %1
 :next
 cd %1
+:dotted
 if not exist %2 md %2
 if not exist %2\data md %2\data
 if not exist %2\evidence md %2\evidence
+if /I "%3" == "" goto skipped
+if not exist %2\csvcur md %2\csvcur
+if not exist %2\csvprev md %2\csvprev
+if not exist %2\mefcur md %2\mefcur
+if not exist %2\mefprev md %2\mefprev
+:skipped
+if /I "%1" == "." goto current 
 cd ..
+:current
 goto end
 :usage
-@echo "Usage:  dircode account quarter/year"
-@echo "Where account is the account name using underscore instead of space"
-@echo "Where quarter/year is in as e.g. 4Q16"
+@echo Usage:  dircode account quarter/year genpar
+@echo Where account is the account name using underscore instead of space
+@echo Where quarter/year is in as e.g. 4Q17
+@echo Where genpar is optional if account is URT, it creates directories for genpar.
 :end
