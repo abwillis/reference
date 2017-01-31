fec0c458e46000cb68bdd37a06e8eb2983b24a21
Andy Willis <awillis1@us.ibm.com>
2017-01-31 11:05:25 -0600

    Created OS version of devices cleanup and minor cleanup of code on storage version.

--- dvmgos.cmd
+++ dvmgos.cmd
@@ -1,4 +1,4 @@
-@REM Version 1.2 MGR-DEVICES cleanup storage
+@REM Version 1.2 MGR-DEVICES cleanup OS
 @echo off
 if exist *MGR* dir /b *MGR* >files.txt
 echo %cd%>>files.txt
@@ -8,11 +8,15 @@ goto text
 
 :filenames
 if [%1]==[] set /P filename=Enter filename 
-if [%2]==[] set /P account=Enter account 
-findstr /I storage %filename% >%account%-DEVICES.csv
+if [%2]==[] set /P account=Enter account  
+findstr /I /V "Device Status" %filename%>hold1.txt
+findstr /I /V SHID hold1.txt>hold.txt
+findstr /I /V storage hold.txt>%account%-DEVICES.csv
 if [%1]==[] taskkill /fi "WINDOWTITLE eq files.txt*" >nul
 if [%2]==[] taskkill /fi "WINDOWTITLE eq files.txt*" >nul
 del files.txt
+del hold.txt
+del hold1.txt
 goto end
 
 
@@ -22,5 +26,6 @@ if [%1]==[] start "" notepad files.txt
 if [%1]==[] goto filenames
 if [%2]==[] start "" notepad files.txt
 goto filenames
+
 
 :end
